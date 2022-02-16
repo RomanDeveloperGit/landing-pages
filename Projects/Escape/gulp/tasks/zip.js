c2dc015c61d53832a1998516e0d6f63d0e87eb86
roman_orlov <roman.developer@mail.ru>
2022-02-16 10:37:11 +0500

    Update directories

--- Projects/Escape/gulp/tasks/zip.js
+++
@@ -1,9 +0,0 @@
-import createrZIP from "gulp-zip";
-
-export const zip = () => {
-	app.plugins.del( `./${app.path.rootFolder}.zip` );
-
-	return app.gulp.src( `${app.path.buildFolder}/**/*.*` )
-		.pipe( createrZIP( `${app.path.rootFolder}.zip` ) )
-		.pipe( app.gulp.dest( "./" ) );
-}
