494c510dd8401470b02d7bdec58366e478fc87d7
roman_orlov <roman.developer@mail.ru>
2022-02-17 23:01:16 +0500

    Update travel project( no adaptive )
    ~ 6 hours

--- traveland/src/js/index.js
+++ traveland/src/js/index.js
@@ -1,1 +1,14 @@
 isWebp();
+
+const swiper = new Swiper( ".offer-swiper", {
+	slidesPerView: 3,
+	allowTouchMove: false, // временно, на телефоне включить
+	pagination: {
+		el: '.offer-swiper__pagination',
+		clickable: true,
+	},
+	navigation: {
+		nextEl: '.offer-swiper__button-next',
+		prevEl: '.offer-swiper__button-prev',
+	},
+});
