f933a574ea5178f9f8c6d8ed6db0d9798bf0a31a
roman_orlov <roman.developer@mail.ru>
2022-02-18 17:03:55 +0500

    Finished the pet-project Traveland

--- traveland/dist/js/index.js
+++ traveland/dist/js/index.js
@@ -10082,8 +10082,8 @@ var isWebp = function isWebp() {
 
 isWebp();
 var swiper = new Swiper(".offer-swiper", {
-  slidesPerView: 3,
-  allowTouchMove: false,
+  slidesPerView: 1,
+  allowTouchMove: true,
   pagination: {
     el: '.offer-swiper__pagination',
     clickable: true
@@ -10091,5 +10091,43 @@ var swiper = new Swiper(".offer-swiper", {
   navigation: {
     nextEl: '.offer-swiper__button-next',
     prevEl: '.offer-swiper__button-prev'
+  },
+  breakpoints: {
+    869: {
+      slidesPerView: 3,
+      allowTouchMove: false
+    },
+    601: {
+      slidesPerView: 2,
+      allowTouchMove: false
+    }
   }
+});
+
+function setStatusMenu() {
+  document.querySelector(".menu-burger").classList.toggle("menu-burger_actived");
+  document.querySelector(".menu").classList.toggle("menu_actived");
+  document.querySelector("body").classList.toggle("no-scroll");
+}
+
+document.querySelector(".menu-burger").onclick = function () {
+  setStatusMenu();
+};
+
+document.querySelectorAll(".menu__link").forEach(function (element) {
+  element.onclick = function () {
+    if (getComputedStyle(document.querySelector(".menu-burger")).display != "none") setStatusMenu();
+  };
+});
+document.querySelectorAll(".slowScroll").forEach(function (element) {
+  element.onclick = function (e) {
+    e.preventDefault();
+    var href = this.getAttribute("href").substring(1);
+    var scrollTarget = document.getElementById(href);
+    var elementPosition = scrollTarget.getBoundingClientRect().top;
+    window.scrollBy({
+      top: elementPosition,
+      behavior: 'smooth'
+    });
+  };
 });
--- traveland/src/js/index.js
+++ traveland/src/js/index.js
@@ -1,14 +1,59 @@
 isWebp();
 
 const swiper = new Swiper( ".offer-swiper", {
-	slidesPerView: 3,
-	allowTouchMove: false, // временно, на телефоне включить
+	slidesPerView: 1,
+	allowTouchMove: true,
+
 	pagination: {
 		el: '.offer-swiper__pagination',
 		clickable: true,
 	},
+
 	navigation: {
 		nextEl: '.offer-swiper__button-next',
 		prevEl: '.offer-swiper__button-prev',
 	},
+
+	breakpoints: {
+		869: {
+			slidesPerView: 3,
+			allowTouchMove: false,
+		},
+		601: {
+			slidesPerView: 2,
+			allowTouchMove: false,
+		}
+	}
+});
+
+function setStatusMenu() {
+	document.querySelector( ".menu-burger" ).classList.toggle( "menu-burger_actived" );
+	document.querySelector( ".menu" ).classList.toggle( "menu_actived" );
+
+	document.querySelector( "body" ).classList.toggle( "no-scroll" );
+}
+
+document.querySelector( ".menu-burger" ).onclick = function() {
+	setStatusMenu();
+};
+
+document.querySelectorAll( ".menu__link" ).forEach( element => {
+	element.onclick = function() {
+		if (getComputedStyle( document.querySelector( ".menu-burger" ) ).display != "none") setStatusMenu();
+	}
+});
+
+document.querySelectorAll( ".slowScroll" ).forEach( element => {
+    element.onclick = function(e) {
+        e.preventDefault();
+
+        let href = this.getAttribute( "href" ).substring( 1 );
+        let scrollTarget = document.getElementById( href );
+        let elementPosition = scrollTarget.getBoundingClientRect().top;
+
+        window.scrollBy({
+            top: elementPosition,
+            behavior: 'smooth'
+        });
+	}
 });
