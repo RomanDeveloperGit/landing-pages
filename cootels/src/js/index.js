5313ccb757f12f72b319ac1586f8a69625062859
roman_orlov <roman.developer@mail.ru>
2022-02-20 00:21:29 +0500

    Cootels project( no adaptive )

--- cootels/src/js/index.js
+++ cootels/src/js/index.js
@@ -1,1 +1,20 @@
 isWebp();
+
+document.querySelectorAll( ".accordion-item" ).forEach( itemElement => {
+	itemElement.addEventListener( "click", () => {
+		document.querySelectorAll( ".accordion-item" ).forEach( _itemElement => {
+			_itemElement.classList.remove( "accordion-item_actived" );
+		});
+
+		itemElement.classList.add( "accordion-item_actived" );
+	});
+});
+
+const swiper = new Swiper( ".swiper", {
+	slidesPerView: 1,
+	loop: true,
+	pagination: {
+		el: '.swiper-pagination',
+		clickable: true,
+	}
+});
