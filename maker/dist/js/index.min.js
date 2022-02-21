876594d45d3a0f77e6c7d1c4293ab4858d835d69
roman_orlov <roman.developer@mail.ru>
2022-02-21 18:06:28 +0500

    Create popup video and fix values name surname in the footer

--- maker/dist/js/index.min.js
+++ maker/dist/js/index.min.js
@@ -17,3 +17,20 @@ function checkSupportWebp() {
 }
 
 checkSupportWebp();
+
+function popupListener() {
+  document.querySelector("body").classList.toggle("no-scroll");
+  document.querySelector(".popup-video").classList.toggle("popup-video_actived");
+  var src = document.querySelector(".popup-video__iframe").getAttribute("src");
+
+  if (!document.querySelector(".popup-video").classList.contains("popup-video_actived")) {
+    document.querySelector(".popup-video__iframe").setAttribute("src", src);
+  }
+}
+
+document.querySelector(".popup-video__button").addEventListener("click", function () {
+  popupListener();
+});
+document.querySelector(".popup-video__close-button").addEventListener("click", function () {
+  popupListener();
+});
--- maker/src/js/index.js
+++ maker/src/js/index.js
@@ -1,1 +1,19 @@
 checkSupportWebp();
+
+function popupListener() {
+	document.querySelector( "body" ).classList.toggle( "no-scroll" );
+	document.querySelector( ".popup-video" ).classList.toggle( "popup-video_actived" );
+
+	let src = document.querySelector( ".popup-video__iframe" ).getAttribute( "src" );
+	if (!document.querySelector( ".popup-video" ).classList.contains( "popup-video_actived" )) {
+		document.querySelector( ".popup-video__iframe" ).setAttribute( "src", src );
+	}
+}
+
+document.querySelector( ".popup-video__button" ).addEventListener( "click", function() {
+	popupListener();
+});
+
+document.querySelector( ".popup-video__close-button" ).addEventListener( "click", function() {
+	popupListener();
+});
